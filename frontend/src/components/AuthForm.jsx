635d0ca1da71120dfc5a14994092f313d4763663
UmeshGardas <umeshgardas123@gmail.com>
2025-10-09 20:33:21 +0530

    updated authform

--- frontend/src/components/AuthForm.jsx
+++ frontend/src/components/AuthForm.jsx
@@ -12,7 +12,7 @@ const AuthForm = ({ onAuthSuccess }) => {
     role: "customer",
   });
 
-  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+  const API_URL = import.meta.env.VITE_API_URL ;
 
   const handleLogin = async (e) => {
     e.preventDefault();
